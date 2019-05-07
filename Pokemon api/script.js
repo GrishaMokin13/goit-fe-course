ebe4d1bdad00e03b1bdc800ad115021ec7be2dcd
GrishaMokin13 <43414790+GrishaMokin13@users.noreply.github.com>
2019-05-07 18:25:30 +0300

    lwnv

--- Pokemon api/script.js
+++ Pokemon api/script.js
@@ -1,13 +1,14 @@
+let wrap;
 fetch(`https://pokeapi.co/api/v2/pokemon/?limit=807%22`)
     .then(poke => poke.json())
     .then(data => data.results)
     .then(arr => {
         for (let j = 0; j < 151; j++) {
-            let name = document.createElement('p');
+            let name = document.createElement('a');
             let img = document.createElement('img');
             let card = document.createElement('div');
             let bool = true;
-            let wrap = document.querySelector('.wrap');
+            wrap = document.querySelector('.wrap');
             card.classList.add('pokemon')
             if (arr[j].name === 'nidoran-f') {
                 arr[j].name = 'nidoranf';
@@ -35,3 +36,83 @@ fetch(`https://pokeapi.co/api/v2/pokemon/?limit=807%22`)
             wrap.append(card);
         }
     });
+let main = document.querySelector('.main');
+main.addEventListener('click', () =>{
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=807%22`)
+    .then(poke => poke.json())
+    .then(data => data.results)
+    .then(arr => {
+        wrap.textContent = '';
+        for (let j = 0; j < 151; j++) {
+            let name = document.createElement('a');
+            let img = document.createElement('img');
+            let card = document.createElement('div');
+            let bool = true;
+            wrap = document.querySelector('.wrap');
+            card.classList.add('pokemon')
+            if (arr[j].name === 'nidoran-f') {
+                arr[j].name = 'nidoranf';
+            }
+            if (arr[j].name === 'nidoran-m') {
+                arr[j].name = 'nidoranm';
+            }
+            img.setAttribute('src', localStorage.getItem(arr[j].name))
+            img.addEventListener('click', () => {
+                if (bool) {
+                    if (arr[j].name === 'nidoranf' || arr[j].name === 'nidoranm' || arr[j].name === 'mr-mime') {} else {
+                        img.setAttribute('src', localStorage.getItem(`${arr[j].name}-shiny`));
+                        bool = !bool;
+                    }
+                } else {
+                    img.setAttribute('src', localStorage.getItem(arr[j].name));
+                    bool = !bool;
+
+                }
+            })
+            name.textContent = arr[j].name;
+            card.append(img, name);
+            wrap.append(card);
+        }
+    });
+})
+randBut = document.querySelector('.random');
+randBut.addEventListener('click', () =>{
+    let num = Math.round(Math.random() * 150);
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=807%22`)
+    .then(poke => poke.json())
+    .then(data => data.results)
+    .then(arr => {
+        
+            let name = document.createElement('a');
+            let img = document.createElement('img');
+            let card = document.createElement('div');
+            let bool = true;
+            let wrap = document.querySelector('.wrap');
+            card.classList.add('pokemon')
+            if (arr[num].name === 'nidoran-f') {
+                arr[num].name = 'nidoranf';
+            }
+            if (arr[num].name === 'nidoran-m') {
+                arr[num].name = 'nidoranm';
+            }
+            img.setAttribute('src', localStorage.getItem(arr[num].name))
+            img.addEventListener('click', () => {
+                if (bool) {
+                    if (arr[num].name === 'nidoranf' || arr[num].name === 'nidoranm' || arr[num].name === 'mr-mime') {} else {
+                        img.setAttribute('src', localStorage.getItem(`${arr[num].name}-shiny`));
+                        bool = !bool;
+                    }
+                } else {
+                    img.setAttribute('src', localStorage.getItem(arr[num].name));
+                    bool = !bool;
+
+                }
+            })
+            wrap.textContent = '';
+            name.textContent = arr[num].name;
+            card.append(img, name);
+            wrap.append(card);
+        
+    });
+        
+})
